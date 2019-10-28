3a0751cf2d0cdde461c9d2c2d6fc4edb0e8caf0b
FredAmartey <fdamartey@gmail.com>
2019-10-28 04:36:52 -0400

    deployment config file

--- vue-chatkit/vue.config.js
+++ vue-chatkit/vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/Vueture/"
-      : "/vueture/vue-chatkit/dist"
+  publicPath: "projects/vueture/vue-chatkit/dist/"
 };
